70489744d333bea34f15616da313a8ed48a47649
Alex Galicio <alexisjoygalicio@gmail.com>
2025-11-02 20:27:57 +0800

    feat: add views

--- main/views.py
+++ main/views.py
@@ -11,16 +11,19 @@ from django.core.paginator import Paginator
 from .forms import ApplicationForm, EmployerProfileForm, JobForm, StudentProfileForm, UserRegistrationForm, LoginForm
 from .models import Application, Saved, EmployerProfile, Job, StudentProfile, UserProfile
 
+
 def home(request):
     # if request.user.is_authenticated:
     #     profile = UserProfile.objects.get(user=request.user)
     #     # redirect to specific page based on user role
     #     return redirect('student_page' if profile.role == 'student' else 'employer_page')
-    
-     # For visitors (not logged in) — show latest 12 featured jobs
-    featured_jobs = Job.objects.filter(status='active').select_related('employer__userprofile__employerprofile').order_by('-created_at')[:9]
+
+    # For visitors (not logged in) — show latest 12 featured jobs
+    featured_jobs = Job.objects.filter(status='active').select_related(
+        'employer__userprofile__employerprofile').order_by('-created_at')[:9]
 
     return render(request, 'main/home.html', {'featured_jobs': featured_jobs})
+
 
 def register(request):
     if request.method == 'POST':
@@ -28,20 +31,23 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('login')
+            profile = UserProfile.objects.get(user=user)
+            return redirect('job_list' if profile.role == 'student' else 'my_jobs')
+
     else:
         form = UserRegistrationForm()
 
     return render(request, 'main/register.html', {'form': form})
 
+
 def custom_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
-        
+
         if form.is_valid():
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-            
+
             user = authenticate(request, username=email, password=password)
             if user is not None:
                 login(request, user)
@@ -51,8 +57,9 @@ def custom_login(request):
                 messages.error(request, 'Invalid username or password.')
     else:
         form = LoginForm()
-        
+
     return render(request, 'main/login.html', {'form': form})
+
 
 def custom_logout(request):
     logout(request)
@@ -64,12 +71,12 @@ def custom_logout(request):
 #     if profile.role != 'student':
 #         messages.error(request, 'Access denied. Student access required.')
 #         return redirect('home')
-    
+
 #     context = {
 #         'user': request.user,
 #         'profile': profile
 #     }
-    
+
 #     return render(request, 'main/student_page.html', context)
 
 # @login_required
@@ -78,12 +85,12 @@ def custom_logout(request):
 #     if profile.role != 'employer':
 #         messages.error(request, 'Access denied. Employer access required.')
 #         return redirect('home')
-    
+
 #     context = {
 #         'user': request.user,
 #         'profile': profile
 #     }
-    
+
 #     return render(request, 'main/employer_page.html', context)
 
 
@@ -94,11 +101,15 @@ def profile_view(request):
 
     # check role and get the specific profile
     if user_profile.role == 'student':
-        profile, created = StudentProfile.objects.get_or_create(user_profile=user_profile)
-        form = StudentProfileForm(request.POST or None, request.FILES or None, instance=profile)
+        profile, created = StudentProfile.objects.get_or_create(
+            user_profile=user_profile)
+        form = StudentProfileForm(
+            request.POST or None, request.FILES or None, instance=profile)
     else:
-        profile, created = EmployerProfile.objects.get_or_create(user_profile=user_profile)
-        form = EmployerProfileForm(request.POST or None, request.FILES or None, instance=profile)
+        profile, created = EmployerProfile.objects.get_or_create(
+            user_profile=user_profile)
+        form = EmployerProfileForm(
+            request.POST or None, request.FILES or None, instance=profile)
 
     # auto fill email for display
     user_email = user.email
@@ -107,7 +118,7 @@ def profile_view(request):
         form.save()
         messages.success(request, 'Profile updated successfully!')
         return redirect('profile')
-    
+
     context = {
         'form': form,
         'role': user_profile.role,
@@ -115,6 +126,7 @@ def profile_view(request):
     }
 
     return render(request, 'main/profile.html', context)
+
 
 @login_required
 def create_job(request):
@@ -126,7 +138,7 @@ def create_job(request):
     # only employer can create job
     if profile.role != 'employer':
         return redirect('home')
-    
+
     employer_profile = EmployerProfile.objects.get(user_profile=profile)
     required_fields = [
         employer_profile.company_name,
@@ -136,12 +148,13 @@ def create_job(request):
         employer_profile.company_size,
         employer_profile.description,
     ]
-    
+
     # complete profile first before creating job
     if not all(required_fields):
-        messages.error(request, "Please complete your employer profile before creating a job.")
+        messages.error(
+            request, "Please complete your profile before creating a job.")
         return redirect('profile')
-    
+
     if request.method == 'POST':
         form = JobForm(request.POST)
         if form.is_valid():
@@ -149,11 +162,12 @@ def create_job(request):
             job.employer = request.user
             job.save()
             messages.success(request, 'Job posting created successfully!')
-            return redirect('home') # TODO: redirect to my jobs page
+            return redirect('my_jobs')
     else:
         form = JobForm()
-    
+
     return render(request, 'main/create_job.html', {'form': form})
+
 
 @login_required
 def edit_job(request, job_id):
@@ -170,24 +184,25 @@ def edit_job(request, job_id):
 
     return render(request, 'main/create_job.html', {
         'form': form,
-        'is_edit': True,  # 👈 pass flag to template
+        'is_edit': True,
     })
 
-# public view
-def job_list(request):
+
+def job_list(request):  # public view
     jobs = Job.objects.filter(status='active').order_by('-created_at')
 
     saved_jobs = []
     if request.user.is_authenticated:
-        saved_jobs = Saved.objects.filter(user=request.user).values_list('job_id', flat=True)
+        saved_jobs = Saved.objects.filter(
+            user=request.user).values_list('job_id', flat=True)
 
     # filtering logic
     search_query = request.GET.get('search', '')
     location_query = request.GET.get('location', '')
     workplace_query = request.GET.get('workplace', '')
     work_type_query = request.GET.get('work_type', '')
-    date_posted = request.GET.get('date_posted', '') 
-    
+    date_posted = request.GET.get('date_posted', '')
+
     # can search by title, company, desc, summary
     if search_query:
         jobs = jobs.filter(
@@ -196,13 +211,13 @@ def job_list(request):
             Q(job_description__icontains=search_query) |
             Q(summary__icontains=search_query)
         )
-    
+
     if location_query:
         jobs = jobs.filter(location__icontains=location_query)
-    
+
     if workplace_query:
         jobs = jobs.filter(workplace=workplace_query)
-    
+
     if work_type_query:
         jobs = jobs.filter(work_type=work_type_query)
 
@@ -218,12 +233,12 @@ def job_list(request):
             jobs = jobs.filter(created_at__gte=today - timedelta(days=14))
         elif date_posted == "30days":
             jobs = jobs.filter(created_at__gte=today - timedelta(days=30))
-    
+
     # 20 jobs per page
     paginator = Paginator(jobs, 20)
     page_number = request.GET.get('page')
     jobs_page = paginator.get_page(page_number)
-    
+
     context = {
         'jobs': jobs_page,
         'saved_jobs': saved_jobs,
@@ -233,41 +248,55 @@ def job_list(request):
         'work_type_query': work_type_query,
         'date_posted': date_posted,
     }
-    
+
     return render(request, 'main/job_list.html', context)
 
 
 # used in split layout on with job list
 def job_detail_panel(request, job_id):
-    # get job id, retturn 404 if it doesnt exist
+    # get job id, return 404 if it doesn't exist
     job = get_object_or_404(Job, id=job_id)
-    
+
     has_applied = False
+    saved_jobs = []
+
     # check if logged in user have already applied to the job
     if request.user.is_authenticated:
-        has_applied = Application.objects.filter(job=job, applicant=request.user).exists()
+        has_applied = Application.objects.filter(
+            job=job, applicant=request.user).exists()
+        # get list of saved job ids for the current user
+        saved_jobs = list(Saved.objects.filter(
+            user=request.user).values_list('job_id', flat=True))
 
     context = {
         'job': job,
         'has_applied': has_applied,
+        'saved_jobs': saved_jobs,
     }
-    
+
     return render(request, 'main/job_detail_panel.html', context)
 
-# standalone job detail page
-def job_detail_full(request, job_id):
+
+def job_detail_full(request, job_id):  # standalone job detail page
     job = get_object_or_404(Job, id=job_id)
-    
+
     has_applied = False
+    saved_jobs = []
+
     if request.user.is_authenticated:
-        has_applied = Application.objects.filter(job=job, applicant=request.user).exists()
+        has_applied = Application.objects.filter(
+            job=job, applicant=request.user).exists()
+        saved_jobs = list(Saved.objects.filter(
+            user=request.user).values_list('job_id', flat=True))
 
     context = {
         'job': job,
         'has_applied': has_applied,
+        'saved_jobs': saved_jobs,
     }
-    
+
     return render(request, 'main/job_detail_full.html', context)
+
 
 @login_required
 def apply_job(request, job_id):
@@ -279,7 +308,24 @@ def apply_job(request, job_id):
     # only student can apply job
     if profile.role != 'student':
         return redirect('job_list')
-    
+
+    # Check if student has completed their profile
+    student_profile = StudentProfile.objects.get(user_profile=profile)
+    required_fields = [
+        student_profile.first_name,
+        student_profile.last_name,
+        student_profile.phone,
+        student_profile.school,
+        student_profile.course,
+        student_profile.year_level,
+    ]
+
+    # complete profile first before creating job
+    if not all(required_fields):
+        messages.error(
+            request, "Please complete your profile before applying a job.")
+        return redirect('profile')
+
     # get job id, return 404 if it doesnt exist
     job = get_object_or_404(Job, id=job_id)
 
@@ -296,11 +342,12 @@ def apply_job(request, job_id):
             application.job = job
             application.applicant = request.user
             application.save()
-            messages.success(request, "Your application was submitted successfully!")
+            messages.success(
+                request, "Your application was submitted successfully!")
             return redirect('job_detail_full', job_id=job.id)
     else:
         form = ApplicationForm()
-        
+
     context = {
         'form': form,
         'job': job
@@ -308,23 +355,27 @@ def apply_job(request, job_id):
 
     return render(request, 'main/apply_job.html', context)
 
+
 @login_required
 def applied_jobs(request):
-    applications = Application.objects.filter(applicant=request.user).select_related('job', 'job__employer').order_by('-applied_at')
-    
+    applications = Application.objects.filter(applicant=request.user).select_related(
+        'job', 'job__employer').order_by('-applied_at')
+
     search_query = request.GET.get('search', '')
     status_filter = request.GET.get('status', '')
 
-    # can search by title, company
+    # can search by title, company, work type, location
     if search_query:
         applications = applications.filter(
             Q(job__title__icontains=search_query) |
-            Q(job__employer__userprofile__employerprofile__company_name__icontains=search_query)
+            Q(job__employer__userprofile__employerprofile__company_name__icontains=search_query) |
+            Q(job__work_type__icontains=search_query) |
+            Q(job__location__icontains=search_query)
         )
 
     if status_filter:
-        applications = applications.filter(job__status=status_filter)
-        
+        applications = applications.filter(status=status_filter)
+
     # 10 applications per page
     paginator = Paginator(applications, 10)
     page_number = request.GET.get('page')
@@ -335,8 +386,9 @@ def applied_jobs(request):
         'search_query': search_query,
         'status_filter': status_filter,
     }
-    
+
     return render(request, 'main/applied_jobs.html', context)
+
 
 @login_required
 def toggle_save(request, job_id):
@@ -351,6 +403,7 @@ def toggle_save(request, job_id):
 
     return JsonResponse({'is_saved': is_saved})
 
+
 @login_required
 def saved_jobs(request):
     saves = Saved.objects.filter(user=request.user).select_related('job')
@@ -358,29 +411,38 @@ def saved_jobs(request):
     search_query = request.GET.get('search', '')
     status_filter = request.GET.get('status', '')
 
-    # can search by title, company
+    # can search by title, company, location
     if search_query:
         saves = saves.filter(
             Q(job__title__icontains=search_query) |
-            Q(job__employer__userprofile__employerprofile__company_name__icontains=search_query)
+            Q(job__employer__userprofile__employerprofile__company_name__icontains=search_query) |
+            Q(job__location__icontains=search_query)
         )
 
     if status_filter:
         saves = saves.filter(job__status=status_filter)
-        
+
     paginator = Paginator(saves, 10)
     page_number = request.GET.get('page')
     save_page = paginator.get_page(page_number)
 
     saved_job_ids = [save.job.id for save in save_page]
 
+    applied_job_ids = list(
+        Application.objects.filter(
+            applicant=request.user, job_id__in=saved_job_ids)
+        .values_list('job_id', flat=True)
+    )
+
     context = {
         'save_page': save_page,
         'saved_jobs': saved_job_ids,
+        'applied_jobs': applied_job_ids,
         'search_query': search_query,
         'status_filter': status_filter,
     }
     return render(request, 'main/saved_jobs.html', context)
+
 
 @login_required
 def my_jobs(request):
@@ -389,11 +451,12 @@ def my_jobs(request):
     search_query = request.GET.get('search', '')
     status_filter = request.GET.get('status', '')
 
-    # can search by title, company
+    # can search by title, company, work type
     if search_query:
         jobs = jobs.filter(
-            Q(job__title__icontains=search_query) |
-            Q(job__employer__userprofile__employerprofile__company_name__icontains=search_query)
+            Q(title__icontains=search_query) |
+            Q(employer__userprofile__employerprofile__company_name__icontains=search_query) |
+            Q(work_type__icontains=search_query)
         )
 
     if status_filter:
@@ -413,16 +476,18 @@ def my_jobs(request):
 
     return render(request, 'main/my_jobs.html', context)
 
+
 @login_required
 def toggle_job_status(request, job_id):
     job = get_object_or_404(Job, id=job_id, employer=request.user)
 
-    if job.status == 'expired':
+    if job.status == 'closed':
         job.status = 'active'
-        messages.success(request, f"'{job.title}' has been marked as active again.")
+        messages.success(
+            request, f"'{job.title}' has been marked as active again.")
     else:
-        job.status = 'expired'
-        messages.info(request, f"'{job.title}' has been marked as expired.")
+        job.status = 'closed'
+        messages.info(request, f"'{job.title}' has been marked closed.")
 
     job.save()
     return redirect('my_jobs')
@@ -434,18 +499,77 @@ def delete_job(request, job_id):
 
     if request.method == 'POST':
         job.delete()
-        messages.success(request, f'Job "{job.title}" has been deleted successfully.')
+        messages.success(
+            request, f'Job "{job.title}" has been deleted successfully.')
 
     return redirect('my_jobs')
+
 
 @login_required
 def view_applications(request, job_id):
     job = get_object_or_404(Job, id=job_id, employer=request.user)
 
-    applications = Application.objects.filter(job=job).select_related('applicant__userprofile').order_by('-applied_at')
+    applications = Application.objects.filter(job=job).select_related(
+        'applicant__userprofile').order_by('-applied_at')
+
+    search_query = request.GET.get('search', '')
+    status_filter = request.GET.get('status', '')
+
+    # can search by name, email
+    if search_query:
+        applications = applications.filter(
+            Q(applicant__first_name__icontains=search_query) |
+            Q(applicant__last_name__icontains=search_query) |
+            Q(applicant__email__icontains=search_query)
+        )
+
+    if status_filter:
+        applications = applications.filter(status=status_filter)
+
+    paginator = Paginator(applications, 10)
+    page_number = request.GET.get('page')
+    applications_page = paginator.get_page(page_number)
 
     context = {
         'job': job,
-        'applications': applications,
+        'applications': applications_page,
+        'search_query': search_query,
+        'status_filter': status_filter,
     }
     return render(request, 'main/view_applications.html', context)
+
+
+@login_required
+def accept_application(request, app_id):
+    if request.method == 'POST':
+        app = get_object_or_404(Application, id=app_id)
+
+        # if app.job.company != request.user.company:
+        #     messages.error(request, 'You do not have permission to accept this application.')
+        #     return redirect('url')
+
+        app.status = 'accepted'
+        app.save()
+
+        messages.success(
+            request, f'Application from {app.applicant.get_full_name()} has been accepted.')
+
+    return redirect('view_applications', job_id=app.job.id)
+
+
+@login_required
+def reject_application(request, app_id):
+    if request.method == 'POST':
+        app = get_object_or_404(Application, id=app_id)
+
+        # if app.job.company != request.user.company:
+        #     messages.error(request, 'You do not have permission to reject this application.')
+        #     return redirect('url')
+
+        app.status = 'rejected'
+        app.save()
+
+        messages.success(
+            request, f'Application from {app.applicant.get_full_name()} has been rejected.')
+
+    return redirect('view_applications', job_id=app.job.id)
