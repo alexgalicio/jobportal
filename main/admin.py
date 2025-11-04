838edb5494cc1b42841520c34e88fdf4fd831e10
Alex Galicio <alexisjoygalicio@gmail.com>
2025-11-04 09:30:52 +0800

    chore: register UserProfile and Saved models

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Job, StudentProfile, EmployerProfile, Application
+from .models import Job, UserProfile, StudentProfile, EmployerProfile, Application, Saved
 
+admin.site.register(UserProfile)
 admin.site.register(StudentProfile)
 admin.site.register(EmployerProfile)
 admin.site.register(Job)
 admin.site.register(Application)
+admin.site.register(Saved)
